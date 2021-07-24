cbf7b26241fa298ef3126a7f4955dfc2cfe4eaf0
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-07-24 15:44:32 +0200

    adding @swup/a11y-plugin

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 import Swup from "swup";
+import SwupA11yPlugin from "@swup/a11y-plugin";
 import SwupHeadPlugin from "@swup/head-plugin";
 import SwupSlideTheme from "@swup/slide-theme";
 
 const swup = new Swup({
-  plugins: [new SwupHeadPlugin(), new SwupSlideTheme()],
+  plugins: [new SwupA11yPlugin(), new SwupHeadPlugin(), new SwupSlideTheme()],
 });
